0d5ff21c10054f5f6af14f85466814791a5af418
Max Leonard Inden <inden@innoscale.de>
2017-01-06 15:33:57 +0100

    Add group service with sample data

---
+++ src/services/groups.js
@@ -0,0 +1,10 @@
+const groups = {};
+
+const sampleGroups = [
+  { id: '0', name: 'First awesome group' },
+  { id: '1', name: 'Second awesome group' },
+];
+
+groups.get = () => sampleGroups;
+
+export default groups;
