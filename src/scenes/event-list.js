836c140e72cbe12883374d03fa5cbdc4f48670ae
Max Leonard Inden <inden@innoscale.de>
2017-01-09 17:52:36 +0100

    Remove footer component

--- src/scenes/event-list.js
+++ src/scenes/event-list.js
@@ -1,19 +1,10 @@
-import React, { PropTypes } from 'react';
-import { Text, View, Navigator } from 'react-native';
-import { Container, Content } from 'native-base';
-import Footer from '../components/footer.js';
-import TitleBar from '../components/title-bar.js';
-import joinTheme from '../../Themes/join-theme.js';
+import React from 'react';
+import { Text, View } from 'react-native';
 
-const EventList = (props) => {
-  return (
-    <View style={{ paddingTop: 56 }} >
-      <Text> Event List </Text>
-    </View>
-  );
-};
-
-EventList.propTypes = {
-};
+const EventList = () => (
+  <View style={{ paddingTop: 56 }} >
+    <Text> Event List </Text>
+  </View>
+);
 
 export default EventList;
